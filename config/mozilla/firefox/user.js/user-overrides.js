56d1550f6d94e5a725490e170e1af454a6eccc62
Daniel <ddmin216@gmail.com>
2024-06-13 10:31:17 -0400

    Firefox and i3 configurations.

---
+++ config/mozilla/firefox/user.js/user-overrides.js
@@ -0,0 +1,4 @@
+user_pref("browser.startup.page", 1);
+user_pref("browser.startup.homepage", "about:home");
+user_pref("privacy.clearOnShutdown.history", false);   // [DEFAULT: true]
+user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
